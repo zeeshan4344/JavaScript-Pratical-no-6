ace22b0cbb0e269f63ac296e67cb2c12f29bd116
Zeeshan Zahid <zeeshanzahid1221@gmail.com>
2024-07-11 21:22:13 +0500

    succesfully completed

---
+++ script.js
@@ -0,0 +1,47 @@
+// let a = " Hello World ";
+// console.log(a.trim());
+
+
+// let a = "Hello World";
+// a = a.substring(6,11);
+// console.log(a);
+
+
+// let a = "Hello World";
+// a = a.slice(0,5);
+// console.log(a);
+
+
+// let str = "hello-world";
+// let arr = str.split("-");
+// console.log(arr);
+
+
+
+// let arr = ["Apple","Banana","Orange"];
+// console.log(arr.join(""));
+
+
+let alpha = "Hello";
+let index = alpha.indexOf("o");
+console.log(index);
+
+
+// let text = "Hello World World";
+// text = text.lastIndexOf("World");
+// console.log(text);
+
+
+// let variable = "world";
+// let var2 = variable.startsWith("wor");
+// console.log(var2);
+
+
+// let variable = "Hello world";
+// let var2 = variable.endsWith("world");
+// console.log(var2);
+
+
+// let a = "Zeeshan Zahid";
+// a = a.includes("Zahid");
+// console.log(a);
